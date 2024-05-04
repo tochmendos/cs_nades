18bcf7dfd57111e939c8a543a855048dcfc6e611
tach <kun@gitwork.ru>
2024-05-04 22:40:28 +0300

    Добавил js-код для хранения массивов

--- maps/mirage.js
+++ maps/mirage.js
@@ -2,7 +2,7 @@
             el: '#app',
             data() {
                 return {
-                    videos: [
+                    spreads: [
                         { id: 1, title: 'Video 1', link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' },
                         { id: 2, title: 'Video 2', link: 'https://www.youtube.com/watch?v=zBnSJt-s9WE' },
                         { id: 3, title: 'Video 3', link: 'https://www.youtube.com/watch?v=kNt-a2R6UyU' },
